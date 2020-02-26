58ef5cd9923c8b07e69ca6fd6085d1fee3ab14e4
Sylvain Lemoine <sylvain.lem1@gmail.com>
2020-02-26 21:41:46 +0100

    download function

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,19 @@
 module Main where
-
+{-# LANGUAGE OverloadedStrings #-}
 import Lib
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
+import Network.HTTP.Conduit
 
 main :: IO ()
-main = someFunc
+main = do downloadWeekReleases "02/19/2020" "/Users/slemoine/dev/workspace/comicspreviews-parser/march.txt"
+
+previewsworldUrl = "https://www.previewsworld.com/NewReleases/Export?format=txt"
+
+downloadWeekReleases :: String -> FilePath -> IO ()
+downloadWeekReleases date path = do
+    request <- parseRequest (previewsworldUrl ++ "&releaseDate=" ++ date)
+    response <- httpLBS $ request 
+    let body = getResponseBody response
+    L8.writeFile path body
+
