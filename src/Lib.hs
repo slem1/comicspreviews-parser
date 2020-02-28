c2677d407a33e429ebdb178826aa05056e3ee4d5
Sylvain Lemoine <sylvain-externe.lemoine@enedis.fr>
2020-02-28 17:30:45 +0100

    wip

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
+    ( 
+        downloadWeekReleases
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Network.HTTP.Simple
+import Network.HTTP.Conduit
+
+previewsworldUrl = "https://www.previewsworld.com/NewReleases/Export?format=txt"
+
+downloadWeekReleases :: String -> FilePath -> IO ()
+downloadWeekReleases date path = do
+    request <- parseUrlThrow (previewsworldUrl ++ "&releaseDate=" ++ date)
+    response <- httpLBS $ request 
+    let body = getResponseBody response
+    L8.writeFile path body
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+import Lib
+import System.IO.Temp
+import System.IO
+
+--test1 = TestCase (assertEqual "for (foo 3)," (1,2) (1,3))
+
+main :: IO Counts
+main = runTestTT tests
+
+tests = TestList [TestLabel "testDownloadWeekReleases" testDownloadWeekReleases]
+
+testDownloadWeekReleases = TestCase $ do
+    result <- downloadAndReadSize
+    assertBool "testDownloadWeekReleases" $ result /= 0    
+    where 
+        downloadAndReadSize = createTempFile >>= \path -> download path >> readSize path
+        createTempFile = emptySystemTempFile "tempComicsPreviewsTest" 
+        download tempFilePath = downloadWeekReleases "02/19/2020" tempFilePath
+        readSize tempFilePath = openFile tempFilePath ReadMode >>= \handle -> hFileSize handle
