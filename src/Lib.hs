630aa6b01c1648155424686e9b3311f19458dd46
slem1 <sylvain.lem1@gmail.com>
2020-02-29 15:39:29 +0100

    readerT

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,18 +8,28 @@ import Network.HTTP.Simple
 import Network.HTTP.Conduit
 import Control.Logging
 import qualified Data.Text as T
+import qualified Data.Configurator as DC
+import Data.Configurator.Types as DC_T
+import Control.Monad.Trans.Reader
 
 previewsworldUrl = "https://www.previewsworld.com/NewReleases/Export?format=txt"
 logFilePath = "/home/slemoine/dev/workspace/comicspreviews-parser/out.log"
 
-downloadWeekReleases :: String -> FilePath -> IO ()
-downloadWeekReleases date path = 
-    --withFileLogging logFilePath $ 
-    do
-    let url = previewsworldUrl ++ "&releaseDate=" ++ date
+downloadWeekReleases :: String -> FilePath -> ReaderT DC_T.Config IO ()
+downloadWeekReleases date path = ask >>= (\config -> return $ do    
+    property <- DC.require config (T.pack "previewsworld_url")
+    let url = property ++ "&releaseDate=" ++ date
     request <- parseUrlThrow url
     response <- httpLBS $ request 
-   -- log' $ T.pack (url ++ ":" ++ (show $ getResponseStatusCode response))
     let body = getResponseBody response
-    L8.writeFile path body
+    L8.writeFile path body)
+-- log' $ T.pack (url ++ ":" ++ (show $ getResponseStatusCode response))        
+--withFileLogging logFilePath $ 
  --   log' $ T.pack ("download from " ++ url ++ " to " ++ path)
+
+loadConfig :: IO ()
+loadConfig = do
+    config <- load'
+    value <- DC.require config (T.pack "previewsworld_url")
+    putStrLn value
+    where load' = DC.load $ [DC.Required "/home/slemoine/dev/workspace/comicspreviews-parser/application.properties"] 
