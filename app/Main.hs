ab19ba863e93366f72486d5de64f67668e18e34c
slem1 <sylvain.lem1@gmail.com>
2020-02-29 14:56:26 +0100

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 import Lib
+import qualified Data.Configurator as DC
+import qualified Data.Text as T
 
 main :: IO ()
-main = do downloadWeekReleases "02/19/2020" "/Users/slemoine/dev/workspace/comicspreviews-parser/march.txt"
+main = do downloadWeekReleases "02/19/2020" "/home/slemoine/dev/workspace/comicspreviews-parser/march.txt"
 
-
+loadConfig :: IO ()
+loadConfig = do
+    config <- load'
+    value <- DC.require config (T.pack "previewsworld_url")
+    putStrLn value
+    where load' = DC.load $ [DC.Required "/home/slemoine/dev/workspace/comicspreviews-parser/application.properties"] 
--- src/Lib.hs
+++ src/Lib.hs
@@ -6,12 +6,20 @@ module Lib
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
 import Network.HTTP.Conduit
+import Control.Logging
+import qualified Data.Text as T
 
 previewsworldUrl = "https://www.previewsworld.com/NewReleases/Export?format=txt"
+logFilePath = "/home/slemoine/dev/workspace/comicspreviews-parser/out.log"
 
 downloadWeekReleases :: String -> FilePath -> IO ()
-downloadWeekReleases date path = do
-    request <- parseUrlThrow (previewsworldUrl ++ "&releaseDate=" ++ date)
+downloadWeekReleases date path = 
+    --withFileLogging logFilePath $ 
+    do
+    let url = previewsworldUrl ++ "&releaseDate=" ++ date
+    request <- parseUrlThrow url
     response <- httpLBS $ request 
+   -- log' $ T.pack (url ++ ":" ++ (show $ getResponseStatusCode response))
     let body = getResponseBody response
     L8.writeFile path body
+ --   log' $ T.pack ("download from " ++ url ++ " to " ++ path)
