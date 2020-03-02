371982255b3b01bc1dc5ff20ccadcd1f3673691a
Sylvain Lemoine <sylvain-externe.lemoine@enedis.fr>
2020-03-02 12:47:35 +0100

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 import Lib
 import qualified Data.Text as T
+import qualified Data.Configurator as DC
+import Data.Configurator.Types as DC_T
+import Control.Monad.Trans.Reader
 
 main :: IO ()
-main = do downloadWeekReleases "02/19/2020" "/home/slemoine/dev/workspace/comicspreviews-parser/march.txt"
+main = do
+    config <- loadMainConfig 
+    outputDir <- DC.require config (T.pack "output_dir")
+    let outputFile = outputDir ++ "20201902-catalog.txt" 
+    runReaderT (downloadWeekReleases "02/19/2020" outputFile) config
 
-
+loadMainConfig :: IO DC_T.Config
+loadMainConfig = DC.load $ [DC.Required "application.properties"] 
--- src/Lib.hs
+++ src/Lib.hs
@@ -6,30 +6,24 @@ module Lib
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
 import Network.HTTP.Conduit
-import Control.Logging
+--import Control.Logging
 import qualified Data.Text as T
 import qualified Data.Configurator as DC
 import Data.Configurator.Types as DC_T
 import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Class
 
-previewsworldUrl = "https://www.previewsworld.com/NewReleases/Export?format=txt"
 logFilePath = "/home/slemoine/dev/workspace/comicspreviews-parser/out.log"
 
 downloadWeekReleases :: String -> FilePath -> ReaderT DC_T.Config IO ()
-downloadWeekReleases date path = ask >>= (\config -> return $ do    
+downloadWeekReleases date path = ask >>= (\config -> lift $ do    
     property <- DC.require config (T.pack "previewsworld_url")
     let url = property ++ "&releaseDate=" ++ date
     request <- parseUrlThrow url
     response <- httpLBS $ request 
     let body = getResponseBody response
     L8.writeFile path body)
+
 -- log' $ T.pack (url ++ ":" ++ (show $ getResponseStatusCode response))        
 --withFileLogging logFilePath $ 
  --   log' $ T.pack ("download from " ++ url ++ " to " ++ path)
-
-loadConfig :: IO ()
-loadConfig = do
-    config <- load'
-    value <- DC.require config (T.pack "previewsworld_url")
-    putStrLn value
-    where load' = DC.load $ [DC.Required "/home/slemoine/dev/workspace/comicspreviews-parser/application.properties"] 
