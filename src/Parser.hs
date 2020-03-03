20378617024ee90566e8b8d658913f7ceac04488
Sylvain Lemoine <sylvain-externe.lemoine@enedis.fr>
2020-03-03 18:08:14 +0100

    wip

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,6 +3,26 @@ module Parser (
 ) where
 
 import Text.Parsec
+import Text.Parsec.String (Parser)
 import Text.Parsec.Char
 
-premierPublishers = string 
+garbage :: Parser String
+garbage = manyTill anyChar startTag
+
+startTag :: Parser String
+startTag = do 
+    first <- string "PREMIER PUBLISHERS" 
+    newline 
+    newline 
+    return first
+
+parseContent :: String -> Either ParseError String 
+parseContent content = parse startTag "" content
+
+parseFile :: FilePath -> IO ()
+parseFile = do 
+    content <- readFile 
+    let result = parseContent content
+    case result of 
+        Left r -> putStrLn r
+        _ -> error "error parsing"
